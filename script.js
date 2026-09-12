33447fa3da3e1df5d84220ebc0b1d02040ff425b
Luka-Duoc <luc.ortizr@duocuc.cl>
2026-09-12 00:01:25 -0300

    2-Adicion calculo notas

--- script.js
+++ script.js
@@ -8,28 +8,36 @@ document.addEventListener("DOMContentLoaded", () => {
 
             const nombre = document.getElementById("nombre").value.trim();
             const apellido = document.getElementById("apellido").value.trim();
-            const promedio = parseFloat(document.getElementById("promedio").value);
+            
+            const n1 = parseFloat(document.getElementById("nota1").value);
+            const n2 = parseFloat(document.getElementById("nota2").value);
+            const n3 = parseFloat(document.getElementById("nota3").value);
 
             if (nombre === "" || apellido === "") {
                 alert("El nombre y/o apellido no deben estar vacio.")
                 return;
             };
 
-            if (isNaN(promedio) ||promedio < 1.0 || promedio > 7.0) {
-                alert("El promedio debe estar entre 1.0 a 7.0");
-                return;
-            };
+            const notas = [n1, n2, n3];
+            const validar = notas.every(n => !isNaN(n) && n >= 1.0 && n <= 7.0);
 
+            if (!validar) {
+                alert("Las 3 notas deben ser valores numéricos válidos entre 1.0 y 7.0.");
+                return;
+            }
+
+            const promedio = (n1 * 0.30) + (n2 * 0.40) + (n3 * 0.30)
             const aprobado = promedio >= 4.0;
             const estado = aprobado ? "Aprobado" : "Reprobado";
             const badgeClass = aprobado ? "text-success fw-bold" : "text-danger fw-bold";
 
+            const clasePromedio = promedio < 4.0 ? "text-danger fw-bold" : ""
 
             tabla.innerHTML += ` 
                 <tr>
                     <td> ${nombre} </td>
                     <td> ${apellido} </td>
-                    <td> ${promedio.toFixed(1)} </td>
+                    <td class="${clasePromedio}"> ${promedio.toFixed(1)} </td>
                     <td class="${badgeClass}"> ${estado} </td>
                 </tr>
             `;
